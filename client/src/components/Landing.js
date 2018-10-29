77a3f8ffc0f3979d8cf17a46dea4686948e29395
Andrew Morales <xemlaros@gmail.com>
2018-10-29 00:36:23 -0500

    add player join view

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,15 +1,56 @@
 import React, { Component } from 'react';
+import { getRandomColor } from '../shared/helpers';
 
 import '../styles/Landing.css';
 
 class Landing extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isPlayer: false,
+      color: null,
+    };
+  }
+
+  handleJoin = () => {
+    const color = getRandomColor();
+    this.setState({
+      isPlayer: true,
+      color,
+    });
+  }
+
+  handleColorChange = () => {
+    const color = getRandomColor();
+    this.setState({
+      color,
+    });
+  }
+
   render() {
+    const { isPlayer, color } = this.state;
+    const styles = {
+      backgroundColor: color,
+    };
+
     return (
-      <div className="page">
-        <div className="menu">
-          <div className="option">host</div>
-          <div className="option">join</div>
-        </div>
+      <div className="page" style={styles}>
+        <div className="background" onClick={isPlayer ? this.handleColorChange : null} role="button" tabIndex={0} />
+        {!isPlayer && (
+          <div className="menu">
+            <button type="button" href="https://github.com/acmorales/life-counter">HOST</button>
+            <button type="button" onClick={this.handleJoin}>JOIN</button>
+          </div>
+        )}
+        {isPlayer && (
+          <div>
+            <form className="playerForm">
+              <input className="lifeInput" defaultValue={40} type="number" />
+              <input placeholder="Enter name" autoFocus type="text" />
+            </form>
+          </div>
+        )}
       </div>
     );
   }
