a98c5a98459b98575b70234421619afcebeaa575
Andrew Morales <xemlaros@gmail.com>
2018-11-03 11:09:43 -0500

    build files

---
+++ client/build/precache-manifest.b1ad2a322ac5c5c8adc2464bea4040a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "842940ae96fac8dfa270",
+    "url": "/static/js/main.842940ae.chunk.js"
+  },
+  {
+    "revision": "5fff20bfbf31e33d9572",
+    "url": "/static/js/1.5fff20bf.chunk.js"
+  },
+  {
+    "revision": "842940ae96fac8dfa270",
+    "url": "/static/css/main.a87c45af.chunk.css"
+  },
+  {
+    "revision": "387805ea59d88bb7bb01b12f5684445d",
+    "url": "/index.html"
+  }
+];
